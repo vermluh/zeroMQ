148f84c7d690e6c5b9b0f6421f8fa963a10436c8
Florian Simon <github@verm-uh.de>
2015-02-14 12:02:41 +0100

    moved to 'rbczmq'

--- ruby/demos/HelloWorld_client.0MQ_demo2.rb
+++ ruby/demos/HelloWorld_client.0MQ_demo2.rb
@@ -2,25 +2,25 @@
 
 require 'awesome_print'
 require 'benchmark'
-require 'ffi-rzmq'
+require 'zmq'
 
 context = ZMQ::Context.new
 
 ap "Connecting to hello world server..."
 @requester = context.socket(:REQ)
-#@requester.connect "tcp://localhost:5555"
-@requester.connect "ipc:///tmp/zeroHW"
+@requester.connect "tcp://192.168.178.66:5555"
+#@requester.connect "tcp://127.0.0.1:5555"
+#@requester.connect "ipc:///tmp/zeroHW"
 
-@count = 100000
+@count = 10000
 def exec_demo()
   1.upto @count do |request_nbr|
-#    ap "Sending request #{request_nbr}..."
+    #ap "Sending request #{request_nbr}..."
     @requester.send "Hello"
   
-    reply = ''
     reply = @requester.recv
   
-#    ap "Received reply #{request_nbr}: [#{reply}]"
+    #ap "Received reply #{request_nbr}: [#{reply}]"
   end
 end
 
--- ruby/demos/HelloWorld_server.0MQ_demo.rb
+++ ruby/demos/HelloWorld_server.0MQ_demo.rb
@@ -1,22 +1,19 @@
 #encoding: utf-8
 
-require 'bundler/setup'
 require 'awesome_print'
-require 'ffi-rzmq'
-
+require 'zmq'
 
 context = ZMQ::Context.new
 
 ap "Starting hello world server..."
 responder = context.socket(ZMQ::REP)
-responder.bind "tcp://*:5555"
-#responder.bind "ipc:///tmp/zeroHW"
+#responder.bind "tcp://*:5555"
+responder.bind "ipc:///tmp/zeroHW"
 
 loop do
-  request = ''
-  responder.recv_string(request)
+  request = responder.recv()
 
 #  ap "received Hello"
 #  sleep(1)
-  responder.send_string "World"
+  responder.send "World"
 end
